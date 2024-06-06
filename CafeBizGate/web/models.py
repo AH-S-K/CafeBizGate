44a46460a9edd7ef90dee0da338ef32c99eb98f3
AH_S_K <amirhossein.soleimani.kalahrudi@gmail,com>
2024-06-06 21:08:25 +0430

    تکمیل مدل ها

--- CafeBizGate/web/models.py
+++ CafeBizGate/web/models.py
@@ -1,52 +1,44 @@
 from django.db import models
-from django.core.validators import RegexValidator,MinLengthValidator,MinValueValidator,EmailValidator
+from django.core.validators import RegexValidator, MinLengthValidator, MinValueValidator, EmailValidator
 
-
-class User(models.Model):
-    username = models.CharField(max_length=255, primary_key=True,validators=[RegexValidator(regex="^[a-zA-Z0-9_]+$")])
+class Users(models.Model):
+    username = models.CharField(max_length=255, primary_key=True, unique=True,validators=[RegexValidator(regex="^[a-zA-Z0-9_]+$")])
     full_name = models.CharField(max_length=255, validators=[RegexValidator(regex="^[a-zA-Z ]+$")])
-    email = models.EmailField(max_length=255, unique=True , validators=[EmailValidator()])
-    password = models.CharField(max_length=255 , validators=[MinLengthValidator(8)])
-    phone_number = models.IntegerField()
-
-
-class Admin(models.Model):
-    username = models.CharField(max_length=255, primary_key=True ,validators=[RegexValidator(regex="^[a-zA-Z0-9_]+$")])
     email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator()])
-    password = models.CharField(max_length=255)
-
+    password = models.CharField(max_length=255, unique=True,validators=[MinLengthValidator(8)])
+    phone_number = models.IntegerField(validators=[RegexValidator(r'^\d{11}$' , 'Phone number must be 11 digits' , 'Invalid number')],unique=True)
+class Admins(models.Model):
+    username = models.CharField(max_length=255, unique=True,primary_key=True, validators=[RegexValidator(regex="^[a-zA-Z0-9_]+$")])
+    email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator()])
+    password = models.CharField(max_length=255 , unique=True)
 
 class Orders(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    username = models.ForeignKey(User, on_delete=models.CASCADE , null=True)
+    order_id = models.AutoField(primary_key=True , unique=True)
+    username = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
     products = models.CharField(max_length=255)
-    purchase_amount = models.IntegerField()
-    type = models.BooleanField()
-
+    purchase_amount = models.IntegerField(validators=[MinValueValidator(0)])
+    type = models.BooleanField() #از بولین فیلد استفاده کردم بجای باینری
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True , unique=True)
+    name = models.CharField(max_length=255 , unique=True)
     sugar = models.IntegerField()
     coffee = models.IntegerField()
     flour = models.IntegerField()
     chocolate = models.IntegerField()
     vertical = models.BinaryField()
     price = models.IntegerField()
-    
-    
+
 class Orders_Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True , unique=True)
+    orders_order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-
 class Storage(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
 
-
-class User_Orders(models.Model):
-    users_username = models.ForeignKey(User, on_delete=models.CASCADE)
+class Users_Orders(models.Model):
+    users_username = models.ForeignKey(Users, on_delete=models.CASCADE)
     orders_order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
