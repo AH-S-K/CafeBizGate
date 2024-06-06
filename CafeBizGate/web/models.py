6b331dacce8b7f157c18af8fc7129b8d892e96b3
AH_S_K <amirhossein.soleimani.kalahrudi@gmail,com>
2024-06-06 21:21:51 +0430

    تکمیل مدل ها

--- CafeBizGate/web/models.py
+++ CafeBizGate/web/models.py
@@ -7,21 +7,22 @@ class Users(models.Model):
     email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator()])
     password = models.CharField(max_length=255, unique=True,validators=[MinLengthValidator(8)])
     phone_number = models.IntegerField(validators=[RegexValidator(r'^\d{11}$' , 'Phone number must be 11 digits' , 'Invalid number')],unique=True)
+
 class Admins(models.Model):
     username = models.CharField(max_length=255, unique=True,primary_key=True, validators=[RegexValidator(regex="^[a-zA-Z0-9_]+$")])
     email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator()])
-    password = models.CharField(max_length=255 , unique=True)
+    password = models.CharField(max_length=255 , unique=True ,validators=[MinLengthValidator(8)])
 
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True , unique=True)
     username = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
-    products = models.CharField(max_length=255)
+    products = models.ManyToManyField('Product' ,through='Orders_Product')
     purchase_amount = models.IntegerField(validators=[MinValueValidator(0)])
     type = models.BooleanField() #از بولین فیلد استفاده کردم بجای باینری
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True , unique=True)
-    name = models.CharField(max_length=255 , unique=True)
+    name = models.CharField(max_length=255 , unique=True ,validators=[RegexValidator(regex="^[a-zA-Z0-9 ]+$")])
     sugar = models.IntegerField()
     coffee = models.IntegerField()
     flour = models.IntegerField()
@@ -36,7 +37,7 @@ class Orders_Product(models.Model):
 
 class Storage(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,validators=[RegexValidator(regex="^[a-zA-Z0-9_]+$")], unique=True)
     amount = models.IntegerField()
 
 class Users_Orders(models.Model):
