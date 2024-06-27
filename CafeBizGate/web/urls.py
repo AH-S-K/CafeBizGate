7ffe62def995b630c7159d9437c1b3edfb707ca0
AH-S-K <amirhossein.soleimani.kalahrudi@gmail.com>
2024-06-27 23:13:03 +0330

    Update urls.py

--- CafeBizGate/web/urls.py
+++ CafeBizGate/web/urls.py
@@ -11,4 +11,13 @@ urlpatterns = [
     path('users/', views.user_list, name='user-list'),
     path('delete-user/', views.delete_user, name='delete-user'),
     path('delete-user2/', views.delete_user2, name='delete-user2'),
+    path('management/' , views.management_view , name='management'),
+    path('management/manage-inventory/', views.manage_inventory, name='manage_inventory'),
+    path('management/add_product/', views.add_product, name='add_product'),
+    path('unauthorized/' , views.unauthorized_access , name='unauthorized'),
+
 ]
+
+#برای اضافه  شدن عکس به دیتا بیس
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
