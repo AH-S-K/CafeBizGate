4d74f9387c866745d073866e4dde6916ffddeb99
AH-S-K <amirhossein.soleimani.kalahrudi@gmail.com>
2024-07-03 18:13:52 +0330

    for runserver

--- CafeBizGate/CafeBizGate/settings.py
+++ CafeBizGate/CafeBizGate/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'web/static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
